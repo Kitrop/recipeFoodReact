124d381d13b7d0a955a368748e38d7336eb1de44
Evgeniy Shabolin <steamguardemail@yandex.ru>
2023-06-06 13:54:28 +0300

    add loading and some style

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Recipes.module.css'
+
+const Loading = () => {
+  return (
+    <div className={styles.loader}></div>
+  )
+}
+
+export default Loading
--- src/components/Recipes.tsx
+++ src/components/Recipes.tsx
@@ -2,6 +2,8 @@ import {useSelector} from "react-redux";
 import {RootState} from "../redux/store";
 import {useGetQuestionsQuery} from "../redux/api/api";
 import {Fragment, useEffect} from "react";
+import styles from '../styles/Recipes.module.css';
+import Loading from "./Loading";
 
 const Recipes = () => {
   const formData = useSelector((state: RootState) => state.form)
@@ -10,60 +12,87 @@ const Recipes = () => {
     "type": "public",
     app_id: "d62a4e57",
     app_key: "1422cefdc48bfa37b6ea14d46a65188f",
-  }
+  };
 
-  if (formData.q !== undefined && formData.q !== "") {
-    filterParams.q = formData.q
-  }
-  if ((formData.minIngredients !== undefined && formData.maxIngredients !== undefined) && (formData.minIngredients !== "" && formData.maxIngredients !== "")) {
-    filterParams.countIngr = `${formData.minIngredients}-${formData.maxIngredients}`;
-  }
-  if (formData.diet !== undefined && formData.diet !== "") {
-    filterParams.diet = formData.diet
-  }
-  if (formData.health !== undefined && formData.health !== "") {
-    filterParams.health = formData.health
-  }
-  if (formData.cuisineType !== undefined && formData.cuisineType !== "") {
-    filterParams.cuisineType = formData.cuisineType
-  }
-  if (formData.mealType !== undefined && formData.mealType !== "") {
-    filterParams.mealType = formData.mealType
-  }
-  if (formData.dishType !== undefined && formData.dishType !== "") {
-    filterParams.dishType = formData.dishType
-  }
-  if ((formData.minTime !== undefined && formData.maxTime !== undefined) && (formData.minTime !== "" && formData.maxTime !== "")) {
-    filterParams.time = `${formData.minTime}-${formData.maxTime}`;
-  }
+  const formFields = [
+    {field: "q", param: "q"},
+    {field: "minIngredients", param: "countIngr"},
+    {field: "maxIngredients", param: "countIngr"},
+    {field: "diet", param: "diet"},
+    {field: "health", param: "health"},
+    {field: "cuisineType", param: "cuisineType"},
+    {field: "mealType", param: "mealType"},
+    {field: "dishType", param: "dishType"},
+    {field: "minTime", param: "time"},
+    {field: "maxTime", param: "time"},
+  ];
 
-  console.log(filterParams)
+  formFields.forEach(({field, param}) => {
+    // @ts-ignore
+    const value = formData[field];
+    if (value !== undefined && value !== "") {
+      if (param === "countIngr" || param === "time") {
+        if (!filterParams[param]) {
+          filterParams[param] = `${value}`;
+        } else {
+          filterParams[param] += `-${value}`;
+        }
+      } else {
+        filterParams[param] = value;
+      }
+    }
+  });
+
   const {data, isLoading, error} = useGetQuestionsQuery(filterParams)
-  console.log(useGetQuestionsQuery(filterParams))
+
   const ResultRecipe = () => {
     if (data && data.hits.length > 0) {
       return (
         <Fragment>
-          {data.hits.map((r) => (
-            <div key={r.recipe.label}>
-              <span>{r.recipe.label}</span>
-              <div>
-                <a href={r.recipe.shareAs}>PPPPPPPPPP</a>
-                <img src={r.recipe.image} alt={r.recipe.label + " photo"} />
+          <div className={styles.wrapper}>
+            {data.hits.map((r) => (
+              <div className={styles.container} key={r.recipe.label}>
+                <div>
+                  <img src={r.recipe.image} alt={r.recipe.label + " photo"}/>
+                </div>
+                <span className={styles.label}>{r.recipe.label}</span>
+                <div>
+                  <h3>Cooking time:</h3>
+                  <p>{r.recipe.totalTime}</p>
+                  <h3>Calories:</h3>
+                  <p>{Math.round(r.recipe.calories)}</p>
+                  <h3>Cuisine type:</h3>
+                  <p>{r.recipe.cuisineType}</p>
+                  <h3>Meal type:</h3>
+                  <p>{r.recipe.mealType}</p>
+                  <h3>Dish type:</h3>
+                  <p>{r.recipe.dishType}</p>
+                </div>
+                <div>
+                  <h3>Ingredients:</h3>
+                  {r.recipe.ingredients.map((i) => <div>{i.food}</div>)}
+                </div>
+                <div className={styles.wrapper_btn}>
+                  <a className={styles.button} href={r.recipe.shareAs}>Подробнее</a>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </Fragment>
       );
+    } else if (isLoading) {
+      return (
+        <Loading/>
+      )
     } else {
-      return <div>Ничего не найдено</div>;
+      return <div className={styles.notFound}> Ничего не найдено</div>;
     }
   };
 
 
   return (
     <div>
-      <ResultRecipe />
+      <ResultRecipe/>
     </div>
   )
 }
